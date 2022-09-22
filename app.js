46c88abeb33f39a841de5c146a1c8d4b227e62c9
thai <thaicao542@gmail.com>
2022-09-22 16:06:54 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const res = require("express/lib/response");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.get("/",(req,res) => {
     res.json({message: "Welcome to contact book application."});
 });
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;   
